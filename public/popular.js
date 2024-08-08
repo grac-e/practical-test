99d99eca53e3bace5a234d43c9527241ae01101e
grac-e <adelowotang@gmail.com>
2024-08-08 15:09:02 +0200

    updated functionality

--- public/popular.js
+++ public/popular.js
@@ -2,6 +2,8 @@ document.addEventListener("alpine:init", () => {
     Alpine.data('popularMake', () => ({
       cars: [],
       popularMake: '',
+      message: '',
+      messageType: '',
       newCar: {
         color: '',
         make: '',
@@ -30,19 +32,61 @@ document.addEventListener("alpine:init", () => {
       },
   
       addNewCar() {
+        if (!this.newCar.color || !this.newCar.make || !this.newCar.model || !this.newCar.reg_number) {
+          this.message = 'Please fill in all fields correctly.';
+          this.messageType = 'error';
+          this.clearMessage();
+          return;
+        }
+
+        const existingCar = this.cars.find(car => car.reg_number === this.newCar.reg_number);
+        if (existingCar) {
+          this.message = 'Error: Registration number is already in use!';
+          this.messageType = 'error';
+          this.clearMessage();
+          return;
+        }
+
         axios.post(`/api/cars`, this.newCar)
           .then(response => {
             this.cars.push(response.data);
+            this.message = 'Entry added successfully!';
+            this.messageType = 'success';
             this.newCar = { color: '', make: '', model: '', reg_number: '' };
+            this.clearMessage();
           })
+          .catch(error => {
+            this.message = 'Failed to add car. Please try again.';
+            this.messageType = 'error';
+            this.clearMessage();
+          });
       },
   
       deleteCarEntry() {
         axios.delete(`/api/cars/${this.deleteCar.reg_number}`)
           .then(response => {
-            this.cars = this.cars.filter(car => car.reg_number !== this.deleteCar.reg_number);
+            if (response.data.message === 'Car entry deleted successfully') {
+              this.cars = this.cars.filter(car => car.reg_number !== this.deleteCar.reg_number);
+              this.message = 'Car entry deleted successfully!';
+              this.messageType = 'success';
+            } else {
+              this.message = 'Car details not found!';
+              this.messageType = 'error';
+            }
             this.deleteCar = { reg_number: '' };
+            this.clearMessage();
           })
-      }
+          .catch(error => {
+            this.message = 'Please input a valid registration number.';
+            this.messageType = 'error';
+            this.clearMessage();
+          });
+      },
+
+      clearMessage() {
+        setTimeout(() => {
+          this.message = '';
+        }, 5000);
+      },
     }));
   });
