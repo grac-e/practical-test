7ee6395e6bbf0fd7a0b9eeca75756dc71a5cb0ba
grac-e <adelowotang@gmail.com>
2024-08-08 15:48:25 +0200

    updated functionality

--- public/popular.js
+++ public/popular.js
@@ -1,92 +1,103 @@
 document.addEventListener("alpine:init", () => {
-    Alpine.data('popularMake', () => ({
-      cars: [],
-      popularMake: '',
-      message: '',
-      messageType: '',
-      newCar: {
-        color: '',
-        make: '',
-        model: '',
-        reg_number: ''
-      },
-      deleteCar: {
-        reg_number: ''
-      },
-  
-      fetchCars() {
-        axios.get(`/api/cars`)
-          .then(response => {
-            this.cars = response.data;
-          })
-      },
-  
-      findMostPopularCar() {
-        axios.get(`/api/popular-car`)
-          .then(response => {
-            this.popularMake = response.data.mostPopularCar;
-            setTimeout(() => {
-              this.popularMake = null;
+  Alpine.data('popularMake', () => ({
+    cars: [],
+    popularMake: '',
+    addMessage: '',
+    addMessageType: '',
+    deleteMessage: '',
+    deleteMessageType: '',
+    newCar: {
+      color: '',
+      make: '',
+      model: '',
+      reg_number: ''
+    },
+    deleteCar: {
+      reg_number: ''
+    },
+
+    fetchCars() {
+      axios.get(`/api/cars`)
+        .then(response => {
+          this.cars = response.data;
+        })
+    },
+
+    findMostPopularCar() {
+      axios.get(`/api/popular-car`)
+        .then(response => {
+          this.popularMake = response.data.mostPopularCar;
+          setTimeout(() => {
+            this.popularMake = null;
           }, 5000);
-          })
-      },
-  
-      addNewCar() {
-        if (!this.newCar.color || !this.newCar.make || !this.newCar.model || !this.newCar.reg_number) {
-          this.message = 'Please fill in all fields correctly.';
-          this.messageType = 'error';
-          this.clearMessage();
-          return;
-        }
+        })
+    },
 
-        const existingCar = this.cars.find(car => car.reg_number === this.newCar.reg_number);
-        if (existingCar) {
-          this.message = 'Error: Registration number is already in use!';
-          this.messageType = 'error';
-          this.clearMessage();
-          return;
-        }
+    addNewCar() {
+      if (!this.newCar.color || !this.newCar.make || !this.newCar.model || !this.newCar.reg_number) {
+        this.addMessage = 'Please fill in all fields correctly.';
+        this.addMessageType = 'error';
+        this.clearAddMessage();
+        return;
+      }
 
-        axios.post(`/api/cars`, this.newCar)
+      const existingCar = this.cars.find(car => car.reg_number === this.newCar.reg_number);
+      if (existingCar) {
+        this.addMessage = 'Error: Registration number is already in use!';
+        this.addMessageType = 'error';
+        this.clearMessage();
+        return;
+      }
+
+      axios.post(`/api/cars`, this.newCar)
+        .then(response => {
+          this.cars.push(response.data);
+          this.addMessage = 'Entry added successfully!';
+          this.addMessageType = 'success';
+          this.newCar = { color: '', make: '', model: '', reg_number: '' };
+          this.clearAddMessage();
+        })
+        .catch(error => {
+          this.addMessage = 'Failed to add car. Please try again.';
+          this.addMessageTypeessageType = 'error';
+          this.clearAddMessage();
+        });
+    },
+
+    deleteCarEntry() {
+      axios.delete(`/api/cars/${this.deleteCar.reg_number}`)
           .then(response => {
-            this.cars.push(response.data);
-            this.message = 'Entry added successfully!';
-            this.messageType = 'success';
-            this.newCar = { color: '', make: '', model: '', reg_number: '' };
-            this.clearMessage();
+              if (response.data.message === 'Car deleted successfully') {
+                  this.cars = this.cars.filter(car => car.reg_number !== this.deleteCar.reg_number);
+                  this.deleteMessage = 'Car deleted successfully!';
+                  this.deleteMessageType = 'success';
+                  this.deleteCar.reg_number = '';
+              } else if (response.data.message === 'Car not found') {
+                  this.deleteMessage = 'Car details not found.';
+                  this.deleteMessageType = 'error';
+              }
+              this.clearDeleteMessage();
           })
           .catch(error => {
-            this.message = 'Failed to add car. Please try again.';
-            this.messageType = 'error';
-            this.clearMessage();
+              console.error(error);
+              this.deleteMessage = 'Please input a valid registration number.';
+              this.deleteMessageType = 'error';
+              this.clearDeleteMessage();
           });
-      },
-  
-      deleteCarEntry() {
-        axios.delete(`/api/cars/${this.deleteCar.reg_number}`)
-          .then(response => {
-            if (response.data.message === 'Car entry deleted successfully') {
-              this.cars = this.cars.filter(car => car.reg_number !== this.deleteCar.reg_number);
-              this.message = 'Car entry deleted successfully!';
-              this.messageType = 'success';
-            } else {
-              this.message = 'Car details not found!';
-              this.messageType = 'error';
-            }
-            this.deleteCar = { reg_number: '' };
-            this.clearMessage();
-          })
-          .catch(error => {
-            this.message = 'Please input a valid registration number.';
-            this.messageType = 'error';
-            this.clearMessage();
-          });
-      },
+  },
 
-      clearMessage() {
-        setTimeout(() => {
-          this.message = '';
-        }, 5000);
-      },
-    }));
-  });
+    clearAddMessage() {
+      setTimeout(() => {
+          this.addMessage = '';
+          this.addMessageType = '';
+      }, 5000);
+  },
+
+  clearDeleteMessage() {
+      setTimeout(() => {
+          this.deleteMessage = '';
+          this.deleteMessageType = '';
+      }, 5000);
+  }
+  }));
+});
